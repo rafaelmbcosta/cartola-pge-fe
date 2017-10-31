37434d029d00837024791deef8dd1ad50a8fed46
Rafael Costa <rafael.costa@pge.ce.gov.br>
2017-10-31 18:00:45 -0300

    Partials layout

---
+++ src/app/mock-backend/mock-partial.ts
@@ -0,0 +1,136 @@
+export const PARTIAL: any[] = [
+
+    {
+        "nickname": "Diego Alves",
+        "points": 5.5,
+        "scouts": "FS SG",
+        "position_id": 1,
+        "position": "Goleiro",
+        "team_name": "djmss fc",
+        "player_name": "Daniel Mendes",
+        "team": "FLA",
+        "team_logo": "https://s.glbimg.com/es/sde/f/equipes/2013/12/16/flamengo_45x45.png"
+    },
+    {
+        "nickname": "Thiago Heleno",
+        "points": 8.6,
+        "scouts": "FD FF FS I PEx5 RBx2 SG",
+        "position_id": 3,
+        "position": "Zagueiro",
+        "team_name": "djmss fc",
+        "player_name": "Daniel Mendes",
+        "team": "ATL",
+        "team_logo": "https://s.glbimg.com/es/sde/f/equipes/2015/06/24/atletico-pr_2015_45.png"
+    },
+    {
+        "nickname": "Betão",
+        "points": 5.2,
+        "scouts": "A FS PE",
+        "position_id": 3,
+        "position": "Zagueiro",
+        "team_name": "djmss fc",
+        "player_name": "Daniel Mendes",
+        "team": "AVA",
+        "team_logo": "https://s.glbimg.com/es/sde/f/equipes/2013/12/16/avai_45x45.png"
+    },
+    {
+        "nickname": "Durval",
+        "points": "-",
+        "scouts": "-",
+        "position_id": 3,
+        "position": "Zagueiro",
+        "team_name": "djmss fc",
+        "player_name": "Daniel Mendes",
+        "team": "SPO",
+        "team_logo": "https://s.glbimg.com/es/sde/f/equipes/2015/07/21/sport45.png"
+    },
+    {
+        "nickname": "Gustavo Scarpa",
+        "points": 10,
+        "scouts": "FDx3 FS G PEx5",
+        "position_id": 4,
+        "position": "Meia",
+        "team_name": "djmss fc",
+        "player_name": "Daniel Mendes",
+        "team": "FLU",
+        "team_logo": "https://s.glbimg.com/es/sde/f/equipes/2015/07/21/fluminense_45x45.png"
+    },
+    {
+        "nickname": "Diego",
+        "points": 5.9,
+        "scouts": "FCx2 FFx2 FSx3 I PEx2 RBx3",
+        "position_id": 4,
+        "position": "Meia",
+        "team_name": "djmss fc",
+        "player_name": "Daniel Mendes",
+        "team": "FLA",
+        "team_logo": "https://s.glbimg.com/es/sde/f/equipes/2013/12/16/flamengo_45x45.png"
+    },
+    {
+        "nickname": "Hernanes",
+        "points": 15.9,
+        "scouts": "Ax2 FC FD FFx2 FSx3 PEx3 RBx2",
+        "position_id": 4,
+        "position": "Meia",
+        "team_name": "djmss fc",
+        "player_name": "Daniel Mendes",
+        "team": "SAO",
+        "team_logo": "https://s.glbimg.com/es/sde/f/equipes/2013/12/16/sao_paulo_45x45.png"
+    },
+    {
+        "nickname": "Zé Rafael ",
+        "points": 9,
+        "scouts": "A FCx3 FSx3 I PEx2 RBx3",
+        "position_id": 4,
+        "position": "Meia",
+        "team_name": "djmss fc",
+        "player_name": "Daniel Mendes",
+        "team": "BAH",
+        "team_logo": "https://s.glbimg.com/es/sde/f/equipes/2013/12/16/bahia_45x45.png"
+    },
+    {
+        "nickname": "Júnior Dutra",
+        "points": 0.2,
+        "scouts": "FCx2 FD FF FSx2 PEx5",
+        "position_id": 5,
+        "position": "Atacante",
+        "team_name": "djmss fc",
+        "player_name": "Daniel Mendes",
+        "team": "AVA",
+        "team_logo": "https://s.glbimg.com/es/sde/f/equipes/2013/12/16/avai_45x45.png"
+    },
+    {
+        "nickname": "Henrique Dourado",
+        "points": 0.3,
+        "scouts": "FCx4 FD FF FSx3 PEx3",
+        "position_id": 5,
+        "position": "Atacante",
+        "team_name": "djmss fc",
+        "player_name": "Daniel Mendes",
+        "team": "FLU",
+        "team_logo": "https://s.glbimg.com/es/sde/f/equipes/2015/07/21/fluminense_45x45.png"
+    },
+    {
+        "nickname": "Lucca",
+        "points": 6.8,
+        "scouts": "FCx3 G I PEx3 RB",
+        "position_id": 5,
+        "position": "Atacante",
+        "team_name": "djmss fc",
+        "player_name": "Daniel Mendes",
+        "team": "PON",
+        "team_logo": "https://s.glbimg.com/es/sde/f/equipes/2013/12/16/ponte_preta_45x45.png"
+    },
+    {
+        "nickname": "Claudinei Oliveira",
+        "points": 4.78,
+        "scouts": "",
+        "position_id": 6,
+        "position": "Técnico",
+        "team_name": "djmss fc",
+        "player_name": "Daniel Mendes",
+        "team": "AVA",
+        "team_logo": "https://s.glbimg.com/es/sde/f/equipes/2013/12/16/avai_45x45.png"
+    }
+
+];
--- src/app/partial/partial-list/partial-list.component.ts
+++ src/app/partial/partial-list/partial-list.component.ts
@@ -3,31 +3,30 @@ import { PartialService } from '../partial.service';
 import { Observable } from 'rxjs/Rx';
 import { Partials } from '../partials';
 import { Router } from '@angular/router';
+import { MockPartialsService } from '../../mock-backend/mock-partials.service';
 
 @Component({
   selector: 'app-partial-list',
   templateUrl: './partial-list.component.html',
   styleUrls: ['./partial-list.component.css'],
-  providers: [ PartialService ]
+  providers: [ PartialService, MockPartialsService ]
 })
+
 export class PartialListComponent implements OnInit {
-  //
   partials: Partials[];
   errorMessage: string;
   mode = "Observable";
   position = 1;
-  //
   constructor(
-    private partialService: PartialService,
+    private partialService: MockPartialsService,
     private router: Router
   ) {}
-  //
 
   ngOnInit() {
     let timer = Observable.timer(0, 60*1000)
     timer.subscribe(() => this.getPartials());
   }
-  //
+
   getPartials(){
     this.partialService.getPartials()
         .subscribe(
