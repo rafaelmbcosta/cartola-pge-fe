0d0a98e1160e7240fe736d3897823de26ea6411c
Rafael Costa <rafaelmbcosta@gmail.com>
2018-04-14 19:04:21 -0300

    Captain feature

--- src/app/partial/partial.ts
+++ src/app/partial/partial.ts
@@ -6,6 +6,7 @@ export class Partial {
     public scouts?: string,
     public team_logo?: string,
     public team_name?: string,
-    public player_name?: string
+    public player_name?: string,
+    public captain?: boolean
   ){}
 }
